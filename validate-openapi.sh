41602d86b6cd714ddd53cc7dfd0866ad6dd18cb8
Swapnil Amin <76989674+homosapien14@users.noreply.github.com>
2024-08-01 16:46:29 +0530

    Update validate-openapi.sh

--- validate-openapi.sh
+++ validate-openapi.sh
@@ -5,21 +5,10 @@ FILES=("./api/meta/build/meta.yaml" "./api/registry/build/registry.yaml" "./api/
 
 for FILE in "${FILES[@]}"; do
     if [ -f "$FILE" ]; then
-        echo "Linting $FILE..."
-        OUTPUT=$(openapi lint "$FILE" 2>&1)
-        if [ $? -ne 0 ]; then
-            echo "$OUTPUT"
-            ERRORS=1
-        else
-            echo "$FILE is valid"
-        fi
+        openapi lint "$FILE"  # Use 'lint' instead of 'validate'
+        echo "$FILE is valid"
     else
         echo "$FILE does not exist"
-        ERRORS=1
+        exit 1
     fi
 done
-
-if [ $ERRORS -ne 0 ]; then
-    echo "There were linting errors. Please fix them before merging."
-    exit 1
-fi
